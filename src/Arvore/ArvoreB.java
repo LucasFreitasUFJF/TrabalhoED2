3cfc837ea1ef6969f688b36a440409d50ed7f6a0
Augusto Castilho <castilho@ice.ufjf.br>
2020-11-25 11:01:08 -0300

    Arvore B

--- src/Arvore/ArvoreB.java
+++ src/Arvore/ArvoreB.java
@@ -4,197 +4,103 @@ import Registros.Livro;
 
 public class ArvoreB {
 
+    private int t;
     private NoB raiz;
-    private int m;
-    private int grau;
 
-    /**
-     *
-     * @param m ordem da arvore
-     */
-    public ArvoreB(int m) {
-        this.raiz = null;
-        this.m = m;
-        this.grau = m / 2;
+    public ArvoreB(int t) {
+        t = t;
+        raiz = new NoB(t);
+        raiz.setN(0);
+        raiz.setEhFolha(true);
     }
 
-    public NoB buscar(Livro livro) {
-        return auxBuscar(raiz, livro, 0);
-    }
-
-    private NoB auxBuscar(NoB no, Livro livro, int i) {
-        if (no != null && i < no.getContVal()) {
-            if (livro.getId() < no.getChaves()[i]) {
-                //vai para o filho na posicao 0.
-                return auxBuscar(no.getFilhos()[i], livro, 0);
-            } else if (livro.getId() > no.getChaves()[i]) {
-                if (i < no.getM()) {
-                    //anda para o proximo indice do vetor.
-                    return auxBuscar(no, livro, i + 1);
-                } else {
-                    //vai para o ultimo vetor de filhos
-                    return auxBuscar(no.getFilhos()[i + 1], livro, 0);
-                }
-            } else {
-                return no;
+    // Search chave
+    private NoB busca(NoB x, Livro chave) {
+        int i = 0;
+        if (x == null) {
+            return x;
+        }
+        for (i = 0; i < x.getN(); i++) {
+            if (chave.getId() < x.getChave()[i].getId()) {
+                break;
             }
-        } else {
+            if (chave.getId() == x.getChave()[i].getId()) {
+                return x;
+            }
+        }
+        if (x.isEhFolha()) {
             return null;
-        }
-    }
-
-    public void insere(Livro livro) {
-        if (raiz == null) {
-            NoB no = new NoB(m, null);
-            raiz = no;
         } else {
-            if (buscar(livro) == null) {
-                auxInsere(raiz, livro, 0);
-            }
+            return busca(x.getFilhos()[i], chave);
         }
     }
 
-    private void auxInsere(NoB no, Livro livro, int i) {
-        if (no!=null && no.ehFolha()) {
-            if (i < no.getContVal()) {
-                if (livro.getId() < no.getChaves()[i]) {
-                    if (no.setValor(livro, i) == false) {
-                        cisao(no, livro);
-                    }
-                } else if (livro.getId() > no.getChaves()[i]) {
-                    //if (i < no.getM() - 1) {
-                    auxInsere(no, livro, i + 1);
-                    //} else {
-                    //  cisao(no, livro);
-                    //}
-                } else {
-                    if (no.setValor(livro, i) == false) {
-                        cisao(no, livro);
-                    }
-                }
-            } else {
-                if (no.setValor(livro, i) == false) {
-                    cisao(no, livro);
-                }
-            }
-        } //todo noh a partir daqui possui pelo menos 2 filhos
-        else if (livro.getId() < no.getChaves()[i]) {
-            auxInsere(no.getFilhos()[i], livro, 0);
-        } else if (livro.getId() > no.getChaves()[i]) {
-            if (i < no.getContVal()) {
-                auxInsere(no, livro, i + 1);
-            } else if (i < no.getM() - 1) {
-                auxInsere(no.getFilhos()[i], livro, 0);
-            } else {
-                auxInsere(no.getFilhos()[i + 1], livro, 0);
-
+    private void separa(NoB x, int pos, NoB y) {
+        NoB z = new NoB(t);
+        z.setEhFolha(y.isEhFolha());
+        z.setN(t - 1);
+        for (int j = 0; j < t - 1; j++) {
+            z.getChave()[j] = y.getChave()[j + t];
+        }
+        if (!y.isEhFolha()) {
+            for (int j = 0; j < t; j++) {
+                z.getFilhos()[j] = y.getFilhos()[j + t];
             }
         }
+        y.setN(t - 1);
+        for (int j = x.getN(); j >= pos + 1; j--) {
+            x.getFilhos()[j + 1] = x.getFilhos()[j];
+        }
+        x.getFilhos()[pos + 1] = z;
+
+        for (int j = x.getN() - 1; j >= pos; j--) {
+            x.getChave()[j + 1] = x.getChave()[j];
+        }
+        x.getChave()[pos] = y.getChave()[t - 1];
+        x.setN(x.getN() + 1);
     }
 
-    private void cisao(NoB no, Livro livro) {
-        //faz um vetor com uma posicao a mais, para poder pegar o meio
-        NoB cisao = new NoB(m + 1, no.getPai());
-        for (int i = 0; i < no.getM(); i++) {
-            cisao.setValor(no.getValores()[i], i);
-        }
-
-        //criacao dos vetores auxiliares
-        int k = 0;
-        while (livro.getId() < cisao.getChaves()[k]) {
-            k++;
-        }
-        cisao.setValor(livro, k);
-        NoB cisaoParte1 = new NoB(m, no.getPai());
-        NoB cisaoParte2 = new NoB(m, no.getPai());
-        for (int i = 0, j = (cisao.getM() / 2) + 1; i < no.getM() / 2; i++, j++) {
-            cisaoParte1.setValor(cisao.getValores()[i], i);
-            cisaoParte2.setValor(cisao.getValores()[j], i);
-        }
-
-        //verifica se eh a raiz e faz a cisao
-        if (no.getPai() != null) {
-            k = 0;
-            while (livro.getId() < no.getPai().getChaves()[k]) {
-                k++;
-            }
-
-            if (no.getPai().setValor(cisao.getValores()[cisao.getContVal() / 2], k) == false) {
-                cisao(no.getPai(), cisao.getValores()[cisao.getContVal() / 2]);
-            } else {
-                no.getPai().setFilho(cisaoParte1, k, no.getPai());
-
-                NoB aux = cisaoParte2;
-                NoB aux2 = no.getPai().getFilhos()[k + 1];
-
-                for (int l = k + 1; l <= no.getPai().getContVal(); l++) {
-                    no.getPai().setFilho(aux, l, no.getPai());
-                    if (no.getPai().getFilhos().length >= l + 1) {
-                        aux = no.getPai().getFilhos()[l + 1];
-                        no.getPai().setFilho(aux2, l + 1, no.getPai());
-                        if (no.getPai().getFilhos().length >= l + 2) {
-                            aux2 = no.getPai().getFilhos()[l + 2];
-                        }
-                    }
-                }
-            }
+    public void insere(final Livro key) {
+        NoB r = raiz;
+        if (r.getN() == 2 * t - 1) {
+            NoB s = new NoB(t);
+            raiz = s;
+            s.setEhFolha(false);
+            s.setN(0);
+            s.getFilhos()[0] = r;
+            separa(s, 0, r);
+            insereValor(s, key);
         } else {
-            //seta os valores nos lugares corretos da arvore
-            NoB novaRaiz = new NoB(m, null);
-            novaRaiz.setValor(cisao.getValores()[m / 2], 0);
-            novaRaiz.setFilho(cisaoParte1, 0, novaRaiz);
-            novaRaiz.setFilho(cisaoParte2, 1, novaRaiz);
-            novaRaiz.setFolha(false);
-            raiz = novaRaiz;
-            no = novaRaiz;
+            insereValor(r, key);
         }
     }
 
-    public void remove(Livro livro) {
-        if (buscar(livro) != null) {
-            auxRemover(raiz, livro, 0);
-        }
-    }
+    final private void insereValor(NoB x, Livro k) {
 
-    private void auxRemover(NoB no, Livro livro, int i) {
-        if (no != null) {
-            if (i <= no.getContVal()) {
-                if (livro.getId() < no.getChaves()[i]) {
-                    auxRemover(no.getFilhos()[i], livro, 0);
-                } else if (livro.getId() > no.getChaves()[i]) {
-                    if (i < no.getM()) {
-                        auxRemover(no, livro, i++);
-                    } else {
-                        auxRemover(no.getFilhos()[i + 1], livro, 0);
-                    }
-                } else {
-                    if (!no.removeValor(i)) {
-                        if (i == 0 && !no.ehFolha()) {
-                            //removeu do filho
-                            if (no.getFilhos()[1].getContVal() > grau) {
-                                redistribuicao(no);
-                            }
-                        } else {
-                            //removeu dele mesmo
-                            juncao();
-                        }
-                    }
+        if (x.isEhFolha()) {
+            int i = 0;
+            for (i = x.getN() - 1; i >= 0 && k.getId() < x.getChave()[i].getId(); i--) {
+                if (i + 1 < x.getChave().length) {
+                    x.getChave()[i + 1] = x.getChave()[i];
                 }
             }
+            x.getChave()[i + 1] = k;
+            x.setN(x.getN() + 1);
+        } else {
+            int i = 0;
+            for (i = x.getN() - 1; i >= 0 && k.getId() < x.getChave()[i].getId(); i--) {
+            }
+            ;
+            i++;
+            NoB tmp = x.getFilhos()[i];
+            if (tmp.getN() == 2 * t - 1) {
+                separa(x, i, tmp);
+                if (k.getId() > x.getChave()[i].getId()) {
+                    i++;
+                }
+            }
+            insereValor(x.getFilhos()[i], k);
         }
-    }
-
-    private void redistribuicao(NoB no) {
-        //no.getFilhos()[0].setValor(no.getValores()[0], grau-1);
-        Livro valorPai = no.getValores()[0];
-        Livro valor1 = no.getFilhos()[1].getValores()[0];
-
-        no.trocaValores(valor1, 0);
-        no.getFilhos()[0].trocaValores(valorPai, grau - 1);
-        no.getFilhos()[1].removeValor(0);
-    }
-
-    private void juncao() {
 
     }
 
@@ -202,22 +108,30 @@ public class ArvoreB {
         auxPrint(raiz, 0);
     }
 
-    private void auxPrint(NoB p, int nivel) {
-        if (p != null) {
+    private void auxPrint(NoB x, int nivel) {
+        if (x != null) {
             System.out.print("(" + nivel + ")");
             for (int i = 0; i < nivel; i++) {
                 System.out.print("--");
             }
+        }
 
-            for (int i = 0; i < p.getContVal(); i++) {
-                System.out.print(p.getChaves()[i] + ", ");
-            }
-            System.out.println("");
-
-            for (int j = 0; j < p.getContFilho(); j++) {
-                auxPrint(p.getFilhos()[j], nivel + 1);
+        for (int i = 0; i < x.getChave().length; i++) {
+            System.out.print(x.getChave()[i].getId() + ", ");
+        }
+        System.out.println("");
+        if (!x.isEhFolha()) {
+            for (int i = 0; i < x.getN() + 1; i++) {
+                auxPrint(x.getFilhos()[i], nivel + 1);
             }
         }
     }
 
+    public boolean contem(Livro k) {
+        if (this.busca(raiz, k) != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/Arvore/NoB.java
+++ src/Arvore/NoB.java
@@ -4,132 +4,67 @@ import Registros.Livro;
 
 public class NoB {
 
-    private long chaves[];
-    private Livro valores[];
-    private NoB filhos[];
-    private NoB pai;
-    private int m; //tamanho do vetor
-    private boolean folha;
-    private int grau;
-    private int contVal; //numero de chaves presentes no noh
-    private int contFilhos;
-
-    public NoB(int m, NoB pai) {
-        this.chaves = new long[m - 1];
-        this.valores = new Livro[m - 1];
-        this.filhos = new NoB[m];
-        this.pai = pai;
-        this.m = m - 1;
-        this.folha = true;
-        this.grau = m / 2;
-        this.contVal = 0;
-        this.contFilhos = 0;
+    /**
+     * @param getN the getN to set
+     */
+    public void setN(int n) {
+        this.n = n;
     }
 
-    public int getM() {
-        return m;
+    /**
+     * @return the getN
+     */
+    public int getN() {
+        return n;
     }
 
-    public long[] getChaves() {
-        return chaves;
+    /**
+     * @return the chave
+     */
+    public Livro[] getChave() {
+        return chave;
     }
 
-    public Livro[] getValores() {
-        return valores;
-    }
-
+    /**
+     * @return the getFilhos
+     */
     public NoB[] getFilhos() {
         return filhos;
     }
 
-    public NoB getPai() {
-        return pai;
+    /**
+     * @return the ehFolha
+     */
+    public boolean isEhFolha() {
+        return ehFolha;
+    }
+    private int n;
+    private Livro[] chave;
+    private NoB[] filhos;
+    private boolean ehFolha;
+
+    public NoB(int t) {
+        int m = 2*t;
+        this.ehFolha = true;
+        this.filhos = new NoB[2 * t+1];
+        this.chave = new Livro[m];
     }
 
-    public int getContVal() {
-        return contVal;
-    }
-
-    public int getContFilho() {
-        return contFilhos;
-    }
-
-    public boolean ehFolha() {
-        return folha;
-    }
-
-    public void setFolha(boolean folha) {
-        this.folha = folha;
-    }
-
-    public boolean setValor(Livro livro, int indice) {
-        if (contVal < m) {
-            long auxChaves = livro.getId();
-            Livro auxValores = livro;
-
-            while (indice <= contVal) {
-                long tmpChaves = chaves[indice];
-                Livro tmpVal = valores[indice];
-
-                chaves[indice] = auxChaves;
-                valores[indice] = auxValores;
-
-                auxChaves = tmpChaves;
-                auxValores = tmpVal;
-                indice++;
-            }
-
-            contVal++;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public void setFilho(NoB filho, int indice, NoB pai) {
-        if (filhos == null) {
-            return;
-        } else if (filho == null) {
-            filhos[indice] = filho;
-        } else if (indice < m && indice >= 0) {
-            filhos[indice] = filho;
-            filhos[indice].pai = pai;
-            contFilhos += 1;
-        }
-    }
-
-    public boolean removeValor(int indice) {
-        if (indice == 0 && contVal - 1 < grau && !ehFolha()) {
-            //chave 0 recebe o valor menor que ele
-            chaves[0] = filhos[0].getChaves()[filhos[0].getContVal()];
-            filhos[0].removeValor(filhos[0].getContVal());
-            filhos[0].contVal--;
-            return filhos[0].contVal >= grau;
-        }
-
-        for (int i = indice; i < m; i++) {
-            chaves[i] = chaves[i + 1];
-            valores[i] = valores[i + 1];
-            contVal--;
-        }
-
-        return contVal >= grau;
-    }
-
-    public boolean removeFilho(int indice) {
-        for (int i = indice; i < contFilhos; i++) {
-            if (i + 1 < m) {
-                filhos[i] = filhos[i + 1];
-            } else {
-                filhos[i] = null;
+    public int encontrar(int k) {
+        for (int i = 0; i < this.n; i++) {
+            if (this.getChave()[i].getId() == k) {
+                return i;
             }
         }
-        contFilhos--;
-        return contFilhos >= grau;
+        return -1;
     }
 
-    public void trocaValores(Livro livro, int indice) {
-        chaves[indice] = livro.getId();
-        valores[indice] = livro;
+    ;
+
+    /**
+     * @param ehFolha the ehFolha to set
+     */
+    public void setEhFolha(boolean ehFolha) {
+        this.ehFolha = ehFolha;
     }
 }
